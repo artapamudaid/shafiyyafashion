a27e00c0d863cae3919f4694dd249ca9943b2747
artapamudaid <pamuda.id@gmail.com>
2022-06-03 21:51:27 +0700

    create homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <HeaderShafiyya/>
+    <HeroShafiyya/>
+    <WomenBanner/>
+    <InstagramSection/>
+    <PartnerLogo/>
+    <FooterSection/>
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShafiyya from '@/components/HeaderShafiyya.vue'
+import HeroShafiyya from '@/components/HeroShafiyya.vue'
+import WomenBanner from '@/components/WomenBanner.vue'
+import InstagramSection from '@/components/InstagramSection.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import FooterSection from '@/components/FooterSection.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    HeaderShafiyya,
+    HeroShafiyya,
+    WomenBanner,
+    InstagramSection,
+    PartnerLogo,
+    FooterSection
+}
 }
 </script>
