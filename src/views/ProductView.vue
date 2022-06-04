ae4229678e90af3bad9bb08d2658f22a39dc8f6b
artapamudaid <pamuda.id@gmail.com>
2022-06-04 10:48:39 +0700

    add router-link

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -10,7 +10,9 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="breadcrumb-text product-more">
-                        <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+                        <router-link to="/">
+                          <i class="fa fa-home"></i> Home
+                        </router-link>
                         <span>Detail</span>
                     </div>
                 </div>
@@ -78,6 +80,9 @@
         </div>
     </section>
     <!-- Product Shop Section End -->
+
+    <RelatedProduct/>
+
     <FooterSection/>
    
   </div>
@@ -87,6 +92,7 @@
 // @ is an alias to /src
 import HeaderShafiyya from '@/components/HeaderShafiyya.vue'
 import FooterSection from '@/components/FooterSection.vue'
+import RelatedProduct from '@/components/RelatedProduct.vue'
 
 import carousel from "vue-owl-carousel"
 
@@ -95,6 +101,7 @@ export default {
   components: {
     HeaderShafiyya,
     FooterSection,
+    RelatedProduct,
     carousel
   },
   data() {
